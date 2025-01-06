73417e6dd19976faa794cd1aec98f384c1fb8b69
Shivam <kingshivam2004@gmail.com>
2025-01-06 12:23:53 +0530

    changed index.js

--- index.js
+++ index.js
@@ -202,4 +202,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-// module.exports = app;
+module.exports = app;
